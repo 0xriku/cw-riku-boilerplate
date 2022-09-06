28eb48da0375a2d9a43befa01fe65c4ac12213cd
0xriku <riku@won.dev>
2022-09-06 18:39:37 -0400

    here we go

---
+++ README.md
@@ -0,0 +1,10 @@
+# Riku Boilerplate
+
+This is a boilerplate to quickly build smart contracts in Rust to run inside a Cosmos SDK module on all chains that enable it.
+
+Functionality included:
+- Contract migration associated with `owner`
+- Basic data writes associated with `data`
+
+To-do:
+- Basic queries
---
+++ src/msg.rs
@@ -0,0 +1,24 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {
+  pub data: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+  Write { data: String },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+  Read {},
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct DataResponse {
+  pub data: String,
+}
